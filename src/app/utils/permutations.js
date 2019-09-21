84d5d4108d74199bb2d05166e90c304f7e0f77a4
Sanjay Narayana <sanjay.narayana@hotmail.com>
2019-09-21 11:48:30 +1000

    Refactor make10.js utils and add comments

--- src/app/utils/permutations.js
+++ src/app/utils/permutations.js
@@ -1,18 +1,30 @@
-// Steinhaus-Johnson-Trotter algorithm
+// permutations(items): generates all possible permutations without
+// repetition of the items list in lexicographical order.
+// ---------------------------------------------------------------------------------
+// e.g. items = ['a', 'b', 'c'] would generate all possible permutations
+// of the items list starting from ['a', 'b', 'c'] -> ['c', 'b', 'a'] resulting in:
+// [['a', 'b', 'c'],
+//  ['b', 'a', 'c'],
+//  ['b', 'c', 'a'],
+//  ['a', 'c', 'b'],
+//  ['c', 'a', 'b'],
+//  ['c', 'b', 'a']]
+// ---------------------------------------------------------------------------------
+// Adapted from: Steinhaus-Johnson-Trotter algorithm
 // https://en.wikipedia.org/wiki/Steinhaus%E2%80%93Johnson%E2%80%93Trotter_algorithm
-export const permutations = (numbers) => {
-  if (numbers.length === 1) {
-    return [numbers];
+export const permutations = (items) => {
+  if (items.length === 1) {
+    return [items];
   }
   let perms = [];
   // Get all permutations for numbers without including the first element
-  let tail = permutations(numbers.slice(1));
+  let tail = permutations(items.slice(1));
   for (let i = 0; i < tail.length; i += 1) {
     const sub = tail[i];
     // Insert first number into every possible position of sub-permutation.
     for (let j = 0; j <= sub.length; j += 1) {
       const pre = sub.slice(0, j);
-      const mid = numbers[0];
+      const mid = items[0];
       const post = sub.slice(j);
       perms.push(pre.concat([mid], post));
     }
--- src/app/utils/product.js
+++ src/app/utils/product.js
@@ -1,15 +1,22 @@
 // product(items, r): Generates r-length combinations of items with
 // repetition (akin to generating r-length cartesian products of the
-// items array)
+// items array).
+// -----------------------------------------------------------------
+// e.g. items = ['a', 'b', 'c'], and r = 2 would generate 2-length
+// combinations with repetitions would result in:
+// ['aa', 'ab', 'ac',
+//  'ba', 'bb', 'bc',
+//  'ca', 'cb', 'cc']
 export const product = (items, r) => {
-  // Duplicate items array r times (use it as a placeholder)
+  // Duplicate items array r times (to use as a placeholder)
   let placeholder = Array(r).fill(items);
-  // Run through the placeholder r times and build up the combinations
-  // by 1 item at at time.
+  // Run through the placeholder r times and build up the
+  // combinations by 1 item at at time.
   return placeholder.reduce((acc, curr) => {
     // For every accumulated item,
     return acc.flatMap(a => {
-      // Tack on each of the items to the end of each accumulated item
+      // Tack on each of the items to the end of each accumulated
+      // item
       return curr.map(c => a.concat(c))
     });
   });
