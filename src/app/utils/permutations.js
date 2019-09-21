5fd357a929c2ecefaac176989f1b255a4623d7ab
Sanjay Narayana <sanjay.narayana@hotmail.com>
2019-09-21 12:06:14 +1000

    more informative comments on permutations file

--- src/app/utils/permutations.js
+++ src/app/utils/permutations.js
@@ -13,21 +13,24 @@
 // Adapted from: Steinhaus-Johnson-Trotter algorithm
 // https://en.wikipedia.org/wiki/Steinhaus%E2%80%93Johnson%E2%80%93Trotter_algorithm
 export const permutations = (items) => {
-  if (items.length === 1) {
-    return [items];
-  }
-  let perms = [];
-  // Get all permutations for numbers without including the first element
+  // Base case: If there is a single item,
+  // return the item inside of a list so it
+  // can be later concatenated in General case.
+  if (items.length === 1) return [items];
+  // General case: Recursively call permutations
+  // for items list without the last item.
   let tail = permutations(items.slice(1));
-  for (let i = 0; i < tail.length; i += 1) {
-    const sub = tail[i];
-    // Insert first number into every possible position of sub-permutation.
-    for (let j = 0; j <= sub.length; j += 1) {
+  let perms = [];
+  tail.forEach(sub => {
+    // Then, insert first number into every
+    // possible position of 'sub'-permutation.
+    sub.forEach((_, j) => {
       const pre = sub.slice(0, j);
       const mid = items[0];
       const post = sub.slice(j);
+      // Push each permutation = arranging [pre, mid, post]
       perms.push(pre.concat([mid], post));
-    }
-  }
+    })
+  });
   return perms;
 };
